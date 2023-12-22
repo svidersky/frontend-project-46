6e87ba0acce90a061cba027db926c3805a1aec8d
Leonid Sviderskii <leonid.sviderskii@unity3d.com>
2023-12-21 22:19:35 -0500

    step 6: wip

--- src/app.js
+++ src/app.js
@@ -3,20 +3,23 @@
 import { extname } from 'path';
 import genDiff from './utils/gendiff.js';
 import readFile from './utils/readfile.js';
+import yamlParser from './utils/yamlParser.js';
 
+// eslint-disable-next-line consistent-return
 const app = (filepath1, filepath2) => {
-  const supportedFormats = ['json'];
+  const supportedFormats = ['json', 'yaml', 'yml'];
 
   const fileFormat = extname(filepath1).replace('.', '');
 
   if (!supportedFormats.includes(fileFormat)) {
-    console.log('Unknown file format');
+    console.log(`Unknown file format: ${fileFormat}, add support for it if needed`);
     return null;
   }
 
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
 
+  // eslint-disable-next-line default-case
   switch (fileFormat) {
     case 'json': {
       const json1 = JSON.parse(data1);
@@ -27,9 +30,23 @@ const app = (filepath1, filepath2) => {
       console.log(result);
       return result;
     }
-    default: {
-      console.log('Impossible to compare files');
-      return null;
+    case 'yaml': {
+      const yaml1 = yamlParser(data1);
+      const yaml2 = yamlParser(data2);
+
+      const result = genDiff(yaml1, yaml2);
+
+      console.log(result);
+      return result;
+    }
+    case 'yml': {
+      const yml1 = yamlParser(data1);
+      const yml2 = yamlParser(data2);
+
+      const result = genDiff(yml1, yml2);
+
+      console.log(result);
+      return result;
     }
   }
 };
